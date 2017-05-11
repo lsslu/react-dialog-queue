c3eda26999ff7a78feb690d8a9369f58df8cd489
lss <lsslu@hotmail.com>
2017-05-11 18:25:22 +0800

    add lib folder

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sourcemaps = require('gulp-sourcemaps');
 var less = require('gulp-less');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
 
 gulp.task('build', function(){
   gulp.src([
@@ -14,14 +17,23 @@ gulp.task('build', function(){
       'stage-0'
     ]
   }))
-  .pipe(gulp.dest('./dist/'));
+  .pipe(gulp.dest('./lib/'));
 
 
   gulp.src([
     'src/queue.less'
   ])
-  .pipe(gulp.dest('./dist/'))
+  .pipe(gulp.dest('./lib/'))
   .pipe(less())
+  .pipe(gulp.dest('./lib/'));
+
+
+  browserify({
+    entries: 'src/index.js'
+  })
+  .transform(babelify)
+  .bundle()
+  .pipe(source('react-dialog-queue.js'))
   .pipe(gulp.dest('./dist/'));
   
 });
